00818d1666c4bed747a4d8e4cee03089b7527aa7
patu1234567 <134269374+patu1234567@users.noreply.github.com>
2023-11-28 14:30:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('You are  in AWS  Cloud. THis is TECHESSAY, let me check'); //write a response to the client
   res.end(); //end the response.
-}).listen(6000); //the server object listens on port 6000
+}).listen(2000); //the server object listens on port 2000
